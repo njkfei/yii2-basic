1cd2a4dfa9aa56477ad50b424f672a9a8f42389b
njkfe <297206021@qq.com>
2016-04-14 18:06:42 +0800

    add fileupload

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -9,6 +9,7 @@
 namespace app\models;
 use yii\base\Model;
 use yii\web\UploadedFile;
+use Aws;
 
 class UploadForm extends Model
 {
@@ -27,7 +28,34 @@ class UploadForm extends Model
     public function upload()
     {
         if ($this->validate()) {
-            $this->imageFile->saveAs('uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+          //  $this->imageFile->saveAs('uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+            $this->imageFile->saveAs('c:/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+
+            $config = array(
+                'region' => 'ap-northeast-1',
+                'credentials' => [
+                    'key' => 'AKIAIXGXRS6IHTVI23QQ',
+                    'secret' => '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',
+                ],
+                'version' => 'latest',
+                // 'debug'   => true
+            );
+
+            $sdk = new Aws\Sdk($config);
+
+            $client = $sdk->createS3();
+
+            try {
+                $result = $client->putObject(array(
+                    'Bucket' => 'theme.kkk',
+                    'Key' =>  $this->imageFile->baseName . '.' . $this->imageFile->extension,
+                    'SourceFile' =>'c:/' . $this->imageFile->baseName . '.' . $this->imageFile->extension,
+                    'ContentType' => 'text/plain',
+                    //      'ACL' => 'public-read',
+                ));
+            } catch (S3Exception $e) {
+                echo $e->getMessage() . "\n";
+            }
             return true;
         } else {
             return false;
